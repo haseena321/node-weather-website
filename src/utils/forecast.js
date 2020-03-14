29afacc1e1941e9e770850135593bd90a163edb0
Haseena.L <haseena.l@iv.com>
2020-03-14 11:36:16 +0530

    Add the hign and low temperature

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if(body.error){
             callback('Unable to find location. Try another search', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary +  ' It is currently '  + body.currently.apparentTemperature +  ' degree out. There is a '  + body.currently.precipProbability + '% chance of rain')
+            // console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary +  ' It is currently '  + body.currently.apparentTemperature +  ' degree out. This high today is '+ body.daily.data[0].temperatureHigh + ' with a low of '+body.daily.data[0].temperatureLow + '. There is a '  + body.currently.precipProbability + '% chance of rain')
         }
     })
 }
